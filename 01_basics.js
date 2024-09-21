606c132f7a15703964fed2790b4782d291b49fc7
Shrikant <shindeshrikant583@gmail.com>
2024-09-21 18:22:48 +0530

    new

--- 01_basics.js
+++
@@ -1,7 +0,0 @@
-/* 
-What is Closure ?
-what is implicit function , all types of implicite things in js
-this keyword
-
-
- */
---
+++ todo.js
@@ -0,0 +1,12 @@
+/* 
+What is Closure ?
+what is implicit function , all types of implicite things in js
+this keyword
+implicit return vs explicit return
+immdiatly invoked function expression , why when where - for multiple IIFE we need to use ; to end one execution
+named iife , parameter passing and exucuting multiple iife
+
+
+! how js works and control flow in js is pending - chai aur code 
+
+ */
