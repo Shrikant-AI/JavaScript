cf4057e0c6dead9767a08e1f183d52526697c9df
Shrikant <shindeshrikant583@gmail.com>
2024-09-21 23:05:32 +0530

    m

--- todo.js
+++ todo.js
@@ -9,11 +9,17 @@ what is map?
 for of / for in / for each loop
 what is callback fun
 object destructuring
-
-
 ! how js works and control flow in js is pending - chai aur code 
+* DOM - 
+-innerHTML vs innetContent vs innerText
+-all types of css selectors and their syntax
+-queryselector
+-what is nodelist & HTMLCollection / how differ from array / 
+convert it into array using - Array.from(nodelist)
+-while using querySelectorAll it gives us elements in NodeList or HTMLCollection so we have to     convert them into array first then apply map , reduce , filter
+- firstElementChild / lastElementChild / parentElement / nextElementSibiling / childNodes
 
- */
+*/
 
 //** For of Loop */
 // let arr = [1,2,3,4,5];
@@ -30,25 +36,24 @@ object destructuring
 // for each loop has access to whole array , item and index in the callback function (item,index,array)
 
 //** Reduce */
- let ShoppingCart = [
-    {
-        itemname : "Phone",
-        price : 1000,
-    },
-    {
-        itemname : "Laptop",
-        price : 50000,
-    },
-    {
-        itemname : "Pen",
-        price : 1000,
-    },
-    {
-        itemname : "Headphone",
-        price : 1500,
-    }
- ]
+//  let ShoppingCart = [
+//     {
+//         itemname : "Phone",
+//         price : 1000,
+//     },
+//     {
+//         itemname : "Laptop",
+//         price : 50000,
+//     },
+//     {
+//         itemname : "Pen",
+//         price : 1000,
+//     },
+//     {
+//         itemname : "Headphone",
+//         price : 1500,
+//     }
+//  ]
 
-const totalPrice =  ShoppingCart.reduce((acc , curr)=> acc + curr.price,0)
-
- console.log(totalPrice)
+// const totalPrice =  ShoppingCart.reduce((acc , curr)=> acc + curr.price,0)
+//  console.log(totalPrice)
